674d3bc66207e515fed47a2671ff2143d847424b
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-04 20:04:41 +0700

    save update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-// import logo from './logo.svg';
+import React from 'react'
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
 import './style/App.scss'
 import Layout1 from './layouts/Layout1'
+import Start from './pages/Start'
 import Scene1 from './pages/Scene1'
 
+
 const App = () => {
+
   return (
     <div className="App">
       <Layout1>
-        <Scene1 />
+        <Switch>
+          <Route exact path="/">
+            <Start />
+          </Route>
+          <Route exact path="/scene1">
+            <Scene1 />
+          </Route>
+        </Switch>
       </Layout1>
     </div>
   );
--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 const Start = () => {
   return (
     <div className="start">
-      Start
+      <div>
+        <div>Start</div>
+        <Link to="/scene1">ไปต่อ</Link>
+      </div>
     </div>
   )
 }
