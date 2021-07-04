4c3e0b06c7d1b2be7dead34fe174eb9acf2d70b4
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-05 00:28:53 +0700

    save task

--- src/layouts/Layout1.js
+++ src/layouts/Layout1.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import ButtonBack from '../components/ButtonBack'
 import ButtonSound from '../components/ButtonSound'
+// import UseWindowSmall from '../utilityhooks/useWindowSmall'
 
 const Layout1 = (props) => {
+  // const isSmallScreen = UseWindowSmall()
+  const [layoutWidth, setLayoutWidth] = useState({
+    width: 'auto'
+  })
+
+  useEffect(() => {
+    const handleResize = () => {
+      const winWidth = window.innerWidth
+      if (winWidth > 768) {
+        const winHeight = window.innerHeight
+        const layoutWidth = {
+          width: `${Math.ceil((winHeight*16)/9)}px`
+        }
+        setLayoutWidth(layoutWidth)
+      }
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
   return (
-    <div className="layout1">
+    <div className="layout1" style={layoutWidth}>
       <div className="layout1__button layout1__button--back">
         <ButtonBack />
       </div>
