83baefd23efb199b26061d711aefa8ea5c6a3869
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-09 02:09:32 +0700

    save task

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './style/App.scss'
-import CallPolice from './pages/CallPolice'
+// import CallPolice from './pages/CallPolice'
 // import FriendDead from './pages/FriendDead'
-// import Siren from './pages/Siren'
+import Siren from './pages/Siren'
 
 
 const App = () => {
   return (
     <div className="App">
-      <CallPolice />
+      {/* <CallPolice /> */}
       {/* <FriendDead /> */}
-      {/* <Siren /> */}
+      <Siren />
     </div>
   );
 }
--- src/pages/Siren.js
+++ src/pages/Siren.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 // import {motion} from 'framer-motion'
+import UseWindowSmall from '../utilityhooks/useWindowSmall'
 import Content from '../layouts/Content'
 import ButtonBack from '../components/ButtonBack'
 import ButtonSound from '../components/ButtonSound'
 import ButtonNext from '../components/ButtonNext'
-
+import BgSirenMd from '../image/pages/siren/bg_siren_md.svg'
+import BgSirenMdOn from '../image/pages/siren/bg_siren_md_on.svg'
+import BgSirenSm from '../image/pages/siren/bg_siren_sm.svg'
+import BgSirenSmOn from '../image/pages/siren/bg_siren_sm_on.svg'
 
 const Siren = () => {
+  const isWindowSmall = UseWindowSmall()
+  const [bgStyle, setBgStyle] = useState({})
+
+  useEffect(() => {
+    const backgrounds = isWindowSmall
+    ? { bg1: BgSirenSm, bg2: BgSirenSmOn }
+    : { bg1: BgSirenMd, bg2: BgSirenMdOn }
+
+    let currentBG = 'bg1'
+    const interval = setInterval(() => {
+      if (currentBG === 'bg1') {
+        setBgStyle({
+          backgroundImage: `url(${backgrounds.bg1})`
+        })
+        currentBG = 'bg2'
+      } else {
+        setBgStyle({
+          backgroundImage: `url(${backgrounds.bg2})`
+        })
+        currentBG = 'bg1'
+      }
+    }, 500)
+
+    return () => clearInterval(interval)
+  }, [isWindowSmall])
+
   return (
     <>
       <ButtonBack />
       <ButtonSound />
       <Content>
-        <div className="scene-panel siren">
+        <div className="scene-panel siren" style={bgStyle}>
           <div className="siren__content box-story">
             <p className="box-story__text text-story">ตอนนี้ คุณตกเป็นผู้ต้องสงสัย<br />ในคดีการตายของเพื่อนสนิท</p>
             <div className="box-story__button">
