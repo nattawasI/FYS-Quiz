6fed53398177eaca9383a457b18c98c326be8cf5
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-10 23:29:50 +0700

    remove useState and useEffect

--- src/pages/Siren.js
+++ src/pages/Siren.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import {motion} from 'framer-motion'
 import UseWindowSmall from '../utilityhooks/useWindowSmall'
 import Content from '../layouts/Content'
