554c49fb74c498df6451106ffe07b51e161b2dee
Prapat Prapatsornmanu(Tun) <prapat.psm@gmail.com>
2021-07-15 00:33:29 +0700

    fix: modified dark-room.

--- src/page/DarkRoom.js
+++ src/page/DarkRoom.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useHistory} from 'react-router-dom'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 import UseWindowSmall from '../utilityhook/useWindowSmall'
 import Content from '../layout/Content'
 import ButtonSound from '../component/ButtonSound'
@@ -32,11 +32,11 @@ const sceneVariantSM = {
     opacity: 0,
   },
   show: {
-    y: [158, 158, 158, 38],
-    opacity: [0, 0, 1, 1],
+    y: [196, 196, 196, 38],
+    opacity: [0, 0, 0, 1],
     transition: {
       duration: 3,
-      ease: "easeOut",
+      ease: "easeInOut",
       times: [0, 0.4, 0.7, 1]
     },
   },
@@ -57,17 +57,18 @@ const textVariantMD = {
     }
   },
 }
+
 const textVariantSM = {
   hidden: {
     y: 25,
     opacity: 0,
   },
   show: {
-    y: [25, 0, 0, -200],
+    y: [25, 0, 0, -196],
     opacity: [0, 1, 1, 1],
     transition: {
       duration: 3,
-      ease: "easeOut",
+      ease: "easeInOut",
       times: [0, 0.4, 0.7, 1]
     }
   }
@@ -75,17 +76,25 @@ const textVariantSM = {
 
 const buttonVariant = {
   hidden: {
-    y: -120,
+    y: 60,
     opacity: 0
   },
   show: {
-    y: -220,
+    y: 0,
     opacity: 1,
     transition: {
       delay: 3,
       duration: 1,
     }
   },
+  exit: {
+    y: 100,
+    opacity: 0,
+    transition: {
+      ease: 'easeInOut',
+      duration: 1
+    }
+  }
 }
 
 const containerVariant = {
@@ -101,6 +110,7 @@ const containerVariant = {
   },
   exit: {
     x: '-100vw',
+    opacity: 0,
     transition: {
       ease: 'easeInOut',
       duration: 1
@@ -130,19 +140,8 @@ const DarkRoom = () => {
           exit="exit"
         >
           {
-            !isWindowSmall && <motion.div
-              key="darkroom-figure-01"
-              className="dark-room__figure"
-              variants={sceneVariantMD}
-              initial="hidden"
-              animate="show"
-              exit="exit"
-            >
-              <img className="dark-room__image dark-room__image--pc" src={bgSceneMD} alt="" />
-            </motion.div>
-          }
-          {
-            isWindowSmall && <motion.div
+            isWindowSmall
+            ? <motion.div
               key="darkroom-figure-02"
               className="dark-room__figure"
               initial="hidden"
@@ -150,7 +149,17 @@ const DarkRoom = () => {
               variants={sceneVariantSM}
               onAnimationComplete={ () => allowLinkToNext = true }
             >
-              <img className="dark-room__image dark-room__image--sp" src={bgSceneSM} alt="" />
+              <img className="dark-room__image dark-room__image--sm" src={bgSceneSM} alt="" />
+            </motion.div>
+            : <motion.div
+              key="darkroom-figure-01"
+              className="dark-room__figure"
+              variants={sceneVariantMD}
+              initial="hidden"
+              animate="show"
+              exit="exit"
+            >
+              <img className="dark-room__image dark-room__image--md" src={bgSceneMD} alt="" />
             </motion.div>
           }
           <div key="darkroom-02" className="dark-room__container">
@@ -174,14 +183,16 @@ const DarkRoom = () => {
                     animate="show"
                   >
                     คุณตื่นขึ้นมากลางดึก<br/>แล้วมีเเต่ความมืดสลัว
-                  </motion.div>
-                  <motion.div
-                    className="dark-room__button"
-                    variants={buttonVariant}
-                    initial="hidden"
-                    animate="show"
-                  >
-                    <ButtonNext to="/open-switch" />
+                    <AnimatePresence>
+                      <motion.div
+                        className="dark-room__button"
+                        variants={buttonVariant}
+                        initial="hidden"
+                        animate="show"
+                      >
+                        <ButtonNext to="/open-switch" />
+                      </motion.div>
+                    </AnimatePresence>
                   </motion.div>
                 </div>
             }
