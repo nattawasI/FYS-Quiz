40440ff3c0ec418802509625f29315f362e0ef71
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-17 23:00:33 +0700

    finish Suggestion

---
+++ src/page/Suggestion.js
@@ -0,0 +1,191 @@
+
+import React, {useState} from 'react'
+import {useRouteActionContext} from '../context/RouteContext'
+import {motion, AnimatePresence} from 'framer-motion'
+import {containerVariant} from '../variable/MotionVariant'
+import {useUserStateContext} from '../context/UserContext'
+import UseWindowSmall from '../utilityhook/useWindowSmall'
+import Content from '../layout/Content'
+import ButtonSound from '../component/ButtonSound'
+import ButtonNext from '../component/ButtonNext'
+
+// motion Variant
+const textVariant = {
+  hidden: {
+    opacity: 0,
+    y: 70
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: 'easeInOut',
+      delay: 0.5,
+      duration: 1
+    },
+  },
+  exit: {
+    y: -100,
+    opacity: 0,
+    transition: {
+      type: 'tween',
+    }
+  }
+}
+
+const buttonVariant = {
+  hidden: {
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      ease: 'easeInOut',
+      delay: 1.5,
+      duration: 0.5
+    }
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      type: 'tween',
+    }
+  }
+}
+
+const Suggestion = () => {
+  const {changeCurrentPageContext} = useRouteActionContext()
+  const { friendInfoContext } = useUserStateContext()
+  const isWindowSmall = UseWindowSmall()
+
+  // state
+  const [showScene1, setShowScene1] = useState(true)
+  const [showScene2, setShowScene2] = useState(false)
+  const [showScene3, setShowScene3] = useState(false)
+
+  // function
+  const goToNextPage = () => {
+    changeCurrentPageContext('End')
+  }
+
+  const changeToScene2 = () => {
+    setShowScene1(false)
+    setShowScene2(true)
+  }
+
+  const changeToScene3 = () => {
+    setShowScene2(false)
+    setShowScene3(true)
+  }
+
+  let nextScene = ''
+  const skipScene = () => {
+    if (isWindowSmall) {
+      if (nextScene === 'scene2') {
+        setShowScene1(false)
+        setShowScene2(true)
+      } else if (nextScene === 'scene3') {
+        setShowScene1(false)
+        setShowScene2(false)
+        setShowScene3(true)
+      }
+    }
+  }
+
+  return (
+    <motion.div
+      variants={containerVariant}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
+      <ButtonSound />
+      <Content>
+        <div className="scene-panel call-police" onClick={skipScene}>
+          <div className="box-story call-police__scene call-police__scene--1">
+            <AnimatePresence>
+              {
+                showScene1
+                && <motion.p className="box-story__text text-story"
+                  key="textScene1"
+                  variants={textVariant}
+                  initial="hidden"
+                  animate="show"
+                  exit="exit"
+                  onAnimationComplete={ () => nextScene = 'scene2' }
+                >หากพบอาการเหล่านี้<br />ให้รีบไปพบแพทย์เพื่อเช็กทันที</motion.p>
+              }
+              {
+                !isWindowSmall && showScene1
+                && <motion.div className="box-story__button"
+                    key="buttonNextScene1"
+                    variants={buttonVariant}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                  >
+                    <ButtonNext onClick={changeToScene2} />
+                  </motion.div>
+              }
+            </AnimatePresence>
+          </div>
+          <div className="box-story call-police__scene call-police__scene--2">
+            <AnimatePresence>
+              {
+                showScene2
+                && <motion.p className="box-story__text text-story"
+                  key="textScene2"
+                  variants={textVariant}
+                  initial="hidden"
+                  animate="show"
+                  exit="exit"
+                  onAnimationComplete={ () => nextScene = 'scene3' }
+                >แต่ถ้าไม่มีอาการ<br /><span className="text-story--bigger">คุณยังโชคดี</span></motion.p>
+              }
+              {
+                !isWindowSmall && showScene2
+                && <motion.div className="box-story__button"
+                    key="buttonNextScene2"
+                    variants={buttonVariant}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                  >
+                    <ButtonNext onClick={changeToScene3} />
+                  </motion.div>
+              }
+            </AnimatePresence>
+          </div>
+          <div className="box-story call-police__scene call-police__scene--3">
+            {
+              showScene3
+              && <motion.p
+                  className="box-story__text text-story"
+                  variants={textVariant}
+                  initial="hidden"
+                  animate="show"
+                >
+                  และควรดูแลตัวเองต่อไป<br />ให้ห่างไกล <span className="text-story--bigger">”ฆาตกร"</span> ที่ทำให้เป็นโรคเบาหวาน
+                </motion.p>
+            }
+            {
+              showScene3
+              &&  <motion.div className="box-story__button"
+                    key="buttonNextScene2"
+                    variants={buttonVariant}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                  >
+                    <ButtonNext onClick={goToNextPage} />
+                  </motion.div>
+            }
+          </div>
+        </div>
+      </Content>
+    </motion.div>
+  )
+}
+
+export default Suggestion
+
