7f72e455eb64cc370ecb7c4cfc9c185b2bc47e8e
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-19 01:52:37 +0700

    save task

--- src/page/Investigate.js
+++ src/page/Investigate.js
@@ -1,13 +1,36 @@
 import React, {useState} from 'react'
 import {useRouteActionContext} from '../context/RouteContext'
-import {motion, AnimatePresence} from 'framer-motion'
+import {motion, AnimatePresence, useAnimation} from 'framer-motion'
 import {containerVariant} from '../variable/MotionVariant'
 import UseWindowSmall from '../utilityhook/useWindowSmall'
 import Content from '../layout/Content'
 import ButtonSound from '../component/ButtonSound'
 import ButtonNext from '../component/ButtonNext'
+import ImgPoliceMd from '../image/page/investigate/img_police_md.svg'
+import ImgPoliceSm from '../image/page/investigate/img_police_sm.svg'
+import ImgPhotoMd from '../image/page/investigate/img_photo_md.svg'
+import ImgPhotoSm from '../image/page/investigate/img_photo_sm.svg'
 
 // Motion Variants
+const panelVariant = {
+  initial: {
+    y: 0,
+    transition: {
+      type: 'tween',
+      ease: "easeInOut",
+      duration: 0.7,
+    }
+  },
+  end: {
+    y: '-22%',
+    transition: {
+      type: 'tween',
+      ease: "easeInOut",
+      duration: 0.7,
+    }
+  },
+}
+
 const textVariant = {
   hidden: {
     opacity: 0,
@@ -44,9 +67,13 @@ const Investigate = () => {
   // utility hook
   const isWindowSmall = UseWindowSmall()
 
+  // useAnimation Motion
+  const panelControl = useAnimation()
+
   // state
-  const [skipAnimate, setSkipAnimate] = useState(false)
-  const [animateComplete, setAnimateComplete] = useState(false)
+  // const [skipAnimate, setSkipAnimate] = useState(false)
+  // const [animateComplete, setAnimateComplete] = useState(false)
+  const [animateTable, setAnimateTable] = useState(false)
 
   // function
   const goToNextPage = () => {
@@ -55,14 +82,24 @@ const Investigate = () => {
 
   const touchPanelSm = () => {
     if (isWindowSmall) {
-      if (animateComplete) {
-        goToNextPage()
-      } else {
-        if (!skipAnimate) {
-          setAnimateComplete(true)
-          setSkipAnimate(true)
-        }
-      }
+      // if (animateComplete) {
+      //   goToNextPage()
+      // } else {
+      //   if (!skipAnimate) {
+      //     setAnimateComplete(true)
+      //     setSkipAnimate(true)
+      //   }
+      // }
+    }
+  }
+
+  const toggleAnimateTable = () => {
+    if (animateTable) {
+      panelControl.start('initial')
+      setAnimateTable(false)
+    } else {
+      panelControl.start('end')
+      setAnimateTable(true)
     }
   }
 
@@ -75,11 +112,20 @@ const Investigate = () => {
     >
       <ButtonSound />
       <Content>
-        <div className="scene-panel page-name" onClick={touchPanelSm}>
-          {
-            // render content at here
-          }
-        </div>
+        <motion.div className="investigate" onClick={touchPanelSm}
+          variants={panelVariant}
+          initial="hidden"
+          animate={panelControl}
+        >
+          <div className="investigate__police">
+            <img src={isWindowSmall ? ImgPoliceSm: ImgPoliceMd} alt="ตำรวจ" />
+          </div>
+          <div className="investigate__content">
+            <div className="investigate__photo">
+              <img src={isWindowSmall ? ImgPhotoSm: ImgPhotoMd} alt="รูปถ่าย" />
+            </div>
+          </div>
+        </motion.div>
       </Content>
     </motion.div>
   )
