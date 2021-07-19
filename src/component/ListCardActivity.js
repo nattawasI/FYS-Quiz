446daea3a00d0c2a313c678298be82c5d06be796
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-19 12:20:55 +0700

    save task

---
+++ src/component/ListCardActivity.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import UseWindowSmall from '../utilityhook/useWindowSmall'
+import ImgCardGameMd from '../image/page/investigate/img_card_game_md.svg'
+import ImgCardGameSm from '../image/page/investigate/img_card_game_sm.svg'
+import ImgCardFoodMd from '../image/page/investigate/img_card_food_md.svg'
+import ImgCardFoodSm from '../image/page/investigate/img_card_food_sm.svg'
+import ImgCardExerciseMd from '../image/page/investigate/img_card_exercise_md.svg'
+import ImgCardExerciseSm from '../image/page/investigate/img_card_exercise_sm.svg'
+
+const ListCardActivity = () => {
+  const isWindowSmall = UseWindowSmall()
+
+  return (
+    <ul className="list-card-activity">
+      <li className="list-card-activity__item list-card-activity__item--game">
+        <button type="button" className="list-card-activity__button">
+          <img
+            className="list-card-activity__img list-card-activity__img--game"
+            src={isWindowSmall ? ImgCardGameSm: ImgCardGameMd}
+            alt="ชวนเล่นเกม"
+          />
+          <span className="list-card-activity__text md-show">ชวนเล่นเกม</span>
+        </button>
+      </li>
+      <li className="list-card-activity__item list-card-activity__item--food">
+        <button type="button" className="list-card-activity__button">
+          <img
+            className="list-card-activity__img list-card-activity__img--food"
+            src={isWindowSmall ? ImgCardFoodSm: ImgCardFoodMd}
+            alt="ชวนกิน"
+          />
+          <span className="list-card-activity__text md-show">ชวนกิน</span>
+        </button>
+      </li>
+      <li className="list-card-activity__item list-card-activity__item--exercise">
+        <button type="button" className="list-card-activity__button">
+          <img
+            className="list-card-activity__img list-card-activity__img--exercise"
+            src={isWindowSmall ? ImgCardExerciseSm: ImgCardExerciseMd}
+            alt="ชวนออกกำลังกาย"
+          />
+          <span className="list-card-activity__text md-show">ชวนออกกำลังกาย</span>
+        </button>
+      </li>
+    </ul>
+  )
+}
+
+export default ListCardActivity
--- src/page/Investigate.js
+++ src/page/Investigate.js
@@ -1,12 +1,14 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {useRouteActionContext} from '../context/RouteContext'
 import {motion, AnimatePresence, useAnimation} from 'framer-motion'
 import {containerVariant} from '../variable/MotionVariant'
 import UseWindowSmall from '../utilityhook/useWindowSmall'
 import Content from '../layout/Content'
 import ButtonSound from '../component/ButtonSound'
+import ButtonBack from '../component/ButtonBack'
 import ButtonNext from '../component/ButtonNext'
 import FormYourName from '../component/FormYourName'
+import ListCardActivity from '../component/ListCardActivity'
 import FormYear from '../component/FormYear'
 import ImgPoliceMd from '../image/page/investigate/img_police_md.svg'
 import ImgPoliceSm from '../image/page/investigate/img_police_sm.svg'
@@ -14,25 +16,6 @@ import ImgPhotoMd from '../image/page/investigate/img_photo_md.svg'
 import ImgPhotoSm from '../image/page/investigate/img_photo_sm.svg'
 
 // Motion Variants
-const panelVariant = {
-  initial: {
-    y: 0,
-    transition: {
-      type: 'tween',
-      ease: "easeInOut",
-      duration: 0.7,
-    }
-  },
-  end: {
-    y: '-22%',
-    transition: {
-      type: 'tween',
-      ease: "easeInOut",
-      duration: 0.7,
-    }
-  },
-}
-
 const textVariant = {
   hidden: {
     opacity: 0,
@@ -62,7 +45,29 @@ const buttonVariant = {
   }
 }
 
+const photoVariant = {
+  exit: {
+    opacity: 0,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.7,
+      delay: 1,
+    }
+  }
+}
+
 const contentVariant = {
+  hidden: {
+    opacity: 0
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.7,
+      delay: 1,
+    }
+  },
   exit: {
     opacity: 0,
     transition: {
@@ -78,14 +83,43 @@ const Investigate = () => {
   // utility hook
   const isWindowSmall = UseWindowSmall()
 
+  // Motion Variants
+  const panelVariant = {
+    initial: {
+      y: 0,
+      transition: {
+        type: 'tween',
+        ease: "easeInOut",
+        duration: 0.7,
+      }
+    },
+    end: {
+      y: isWindowSmall ? '-30%': '-22%',
+      transition: {
+        type: 'tween',
+        ease: "easeInOut",
+        duration: 0.7,
+      }
+    },
+  }
+
   // useAnimation Motion
   const panelControl = useAnimation()
 
   // state
   // const [skipAnimate, setSkipAnimate] = useState(false)
   // const [animateComplete, setAnimateComplete] = useState(false)
-  const [animateTable, setAnimateTable] = useState(false)
-  const [scene, setScene] = useState('')
+  const [animateTable, setAnimateTable] = useState(true)
+  const [showPhoto, setShowPhoto] = useState(false)
+  const [sceneYourName, setSceneYourName] = useState(false)
+  const [sceneMurder, setSceneMurder] = useState(false)
+  const [sceneAskCooperation, setSceneAskCooperation] = useState(false)
+  const [sceneActivityOften, setSceneActivityOften] = useState(true)
+  const [sceneQuiz, setSceneQuiz] = useState(false)
+  const [sceneFormYear, setSceneFormYear] = useState(false)
+  const [sceneActivityToday, setSceneActivityToday] = useState(false)
+  const [sceneThankYou, setSceneThankYou] = useState(false)
+  const [sceneCause, setSceneCause] = useState(false)
 
   // function
   const goToNextPage = () => {
@@ -107,21 +141,19 @@ const Investigate = () => {
 
   const toggleAnimateTable = () => {
     if (animateTable) {
-      panelControl.start('initial')
-      setAnimateTable(false)
+      setShowPhoto(true)
+      // panelControl.start('initial')
+      // setAnimateTable(false)
     } else {
-      panelControl.start('end')
-      setAnimateTable(true)
+      setShowPhoto(false)
+      // panelControl.start('end')
+      // setAnimateTable(true)
     }
   }
 
-  const toggleA = () => {
-    if (scene === 'a') {
-      setScene('')
-    } else {
-      setScene('a')
-    }
-  }
+  useEffect(() => {
+    toggleAnimateTable()
+  }, [])
 
   return (
     <motion.div
@@ -130,7 +162,8 @@ const Investigate = () => {
       animate="show"
       exit="exit"
     >
-      <ButtonSound onClick={toggleA} />
+      <ButtonBack />
+      <ButtonSound onClick={toggleAnimateTable} />
       <Content>
         <motion.div className="investigate" onClick={touchPanelSm}
           variants={panelVariant}
@@ -141,78 +174,170 @@ const Investigate = () => {
             <img src={isWindowSmall ? ImgPoliceSm: ImgPoliceMd} alt="ตำรวจ" />
           </div>
           <div className="investigate__space">
-            <div className="investigate__photo">
-              <img src={isWindowSmall ? ImgPhotoSm: ImgPhotoMd} alt="รูปถ่าย" />
-            </div>
+            {
+              showPhoto
+              && <div className="investigate__photo">
+                  <img src={isWindowSmall ? ImgPhotoSm: ImgPhotoMd} alt="รูปถ่าย" />
+                </div>
+            }
             <div className="investigate__content">
-              {/* <FormYourName /> */}
-              {/* <div className="box-story">
-                <motion.p className="box-story__text text-story"
-                  variants={textVariant}
-                  initial="hidden"
-                  animate="show"
-                  exit="exit"
-                  // onAnimationComplete={ () => nextScene = 'scene2' }
-                >ตอนนี้เรากำลังสงสัยว่านี่คือ<br /><span className="text-story--bigger">"คดีฆาตกรรม"</span></motion.p>
-                {
-                  !isWindowSmall
-                  && <motion.div className="box-story__button"
-                      key="buttonNextScene1"
-                      variants={buttonVariant}
-                      initial="hidden"
-                      animate="show"
-                      exit="exit"
-                    >
-                      <ButtonNext />
-                    </motion.div>
-                }
-              </div> */}
-              {/* <div className="box-story">
-                <motion.p className="box-story__text text-story"
-                  variants={textVariant}
-                  initial="hidden"
-                  animate="show"
-                  exit="exit"
-                  // onAnimationComplete={ () => nextScene = 'scene3' }
-                >ขอความร่วมมือ คุณบรอน<br />ในการให้ปากคำด้วยนะครับ</motion.p>
-                {
-                  !isWindowSmall
-                  && <motion.div className="box-story__button"
-                      key="buttonNextScene1"
-                      variants={buttonVariant}
-                      initial="hidden"
-                      animate="show"
-                      exit="exit"
-                    >
-                      <ButtonNext />
-                    </motion.div>
-                }
-              </div> */}
-              {/* <div className="box-story">
-                <motion.p className="box-story__text text-story"
-                  variants={textVariant}
-                  initial="hidden"
-                  animate="show"
-                  exit="exit"
-                  // onAnimationComplete={ () => nextScene = 'scene3' }
-                >ขอบคุณในการให้ปากคำ</motion.p>
-                {
-                  !isWindowSmall
-                  && <motion.div className="box-story__button"
-                      key="buttonNextScene1"
-                      variants={buttonVariant}
-                      initial="hidden"
-                      animate="show"
-                      exit="exit"
-                    >
-                      <ButtonNext />
-                    </motion.div>
-                }
-              </div> */}
               <AnimatePresence>
                 {
-                  scene === 'a'
+                  sceneYourName
                   && <motion.div
+                      key="scene-your-name"
+                      variants={contentVariant}
+                      exit="exit"
+                    >
+                      <FormYourName />
+                    </motion.div>
+                }
+                {
+                  sceneMurder
+                  && <motion.div
+                      key="scene-murdur"
+                      variants={contentVariant}
+                      exit="exit"
+                    >
+                      <div className="box-story">
+                        <motion.p className="box-story__text text-story"
+                          variants={textVariant}
+                          initial="hidden"
+                          animate="show"
+                          exit="exit"
+                          // onAnimationComplete={ () => nextScene = 'scene2' }
+                        >ตอนนี้เรากำลังสงสัยว่านี่คือ<br /><span className="text-story--bigger">"คดีฆาตกรรม"</span></motion.p>
+                        {
+                          !isWindowSmall
+                          && <motion.div className="box-story__button"
+                              key="buttonNextScene1"
+                              variants={buttonVariant}
+                              initial="hidden"
+                              animate="show"
+                              exit="exit"
+                            >
+                              <ButtonNext />
+                            </motion.div>
+                        }
+                      </div>
+                    </motion.div>
+                }
+                {
+                  sceneAskCooperation
+                  && <motion.div
+                      key="scene-thank-you"
+                      variants={contentVariant}
+                      exit="exit"
+                    >
+                      <div className="box-story">
+                        <motion.p className="box-story__text text-story"
+                          variants={textVariant}
+                          initial="hidden"
+                          animate="show"
+                          exit="exit"
+                          // onAnimationComplete={ () => nextScene = 'scene3' }
+                        >ขอความร่วมมือ คุณบรอน<br />ในการให้ปากคำด้วยนะครับ</motion.p>
+                        {
+                          !isWindowSmall
+                          && <motion.div className="box-story__button"
+                              key="buttonNextScene1"
+                              variants={buttonVariant}
+                              initial="hidden"
+                              animate="show"
+                              exit="exit"
+                            >
+                              <ButtonNext />
+                            </motion.div>
+                        }
+                      </div>
+                    </motion.div>
+                }
+                {
+                  sceneActivityOften
+                  && <motion.div
+                      className="form-activity"
+                      key="scene-activity-often"
+                      variants={contentVariant}
+                      initial="hidden"
+                      animate="show"
+                      exit="exit"
+                    >
+                      <div className="text-story form-activity__title">เริ่มจากก่อนเกิดเหตุ กิจกรรมระหว่างคุณ<br />กับคุณ ปิยะบุตร ที่ทำบ่อย ๆ คืออะไร? </div>
+                      <div className="form-activity__list">
+                        <ListCardActivity />
+                      </div>
+                    </motion.div>
+                }
+                {
+                  sceneQuiz
+                  && <motion.div
+                      key="scene-quiz"
+                      variants={contentVariant}
+                      exit="exit"
+                    >
+                      Quiz
+                    </motion.div>
+                }
+                {
+                  sceneFormYear
+                  && <motion.div
+                      key="scene-year"
+                      variants={contentVariant}
+                      exit="exit"
+                    >
+                      <FormYear />
+                    </motion.div>
+                }
+                {
+                  sceneActivityToday
+                  && <motion.div
+                      className="form-activity"
+                      key="scene-activity-today"
+                      variants={contentVariant}
+                      initial="hidden"
+                      animate="show"
+                      exit="exit"
+                    >
+                      <div className="text-story form-activity__title">กิจกรรมสุดท้ายที่รู้สึกว่า<br />ได้ทำร่วมกับเพื่อนสนิท ในวันเกิดเหตุ?</div>
+                      <div className="form-activity__list">
+                        <ListCardActivity />
+                      </div>
+                    </motion.div>
+                }
+                {
+                  sceneThankYou
+                  && <motion.div
+                      key="scene-thank-you"
+                      variants={contentVariant}
+                      exit="exit"
+                    >
+                    <div className="box-story">
+                      <motion.p className="box-story__text text-story"
+                        variants={textVariant}
+                        initial="hidden"
+                        animate="show"
+                        exit="exit"
+                        // onAnimationComplete={ () => nextScene = 'scene3' }
+                      >ขอบคุณในการให้ปากคำ</motion.p>
+                      {
+                        !isWindowSmall
+                        && <motion.div className="box-story__button"
+                            key="buttonNextScene1"
+                            variants={buttonVariant}
+                            initial="hidden"
+                            animate="show"
+                            exit="exit"
+                          >
+                            <ButtonNext />
+                          </motion.div>
+                      }
+                    </div>
+                  </motion.div>
+                }
+                {
+                  sceneCause
+                  && <motion.div
+                        key="scene-cause"
                         variants={contentVariant}
                         exit="exit"
                       >
@@ -240,11 +365,6 @@ const Investigate = () => {
                       </motion.div>
                 }
               </AnimatePresence>
-              {/* <AnimatePresence>
-                {
-                  <FormYear />
-                }
-              </AnimatePresence> */}
             </div>
           </div>
         </motion.div>
