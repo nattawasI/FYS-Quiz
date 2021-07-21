69dab8a8afaff5f8c58debbbf4dddce3488d49aa
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-22 00:54:06 +0700

    save task

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react'
 import './style/App.scss'
 import UserProvider from './context/UserContext'
 import RouteProvider from './context/RouteContext'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Index from './page/Index'
+import Admin from './page/Admin'
 
 const App = () => {
 
@@ -10,7 +16,16 @@ const App = () => {
     <UserProvider>
       <div className="App">
         <RouteProvider>
-          <Index />
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <Index />
+              </Route>
+              <Route path="/admin">
+                <Admin />
+              </Route>
+            </Switch>
+          </Router>
         </RouteProvider>
       </div>
     </UserProvider>
---
+++ src/page/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Admin = () => {
+
+  return (
+    <div>
+      This admin page
+    </div>
+  )
+}
+
+export default Admin
