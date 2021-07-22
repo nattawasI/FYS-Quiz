75186a6e546e45b0c0ef0d26bd0b8cba53a7d805
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-22 15:53:13 +0700

    save

--- src/page/FriendSleep.js
+++ src/page/FriendSleep.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import {useUserStateContext} from '../context/UserContext'
-import {motion, AnimatePresence} from 'framer-motion'
+import {motion} from 'framer-motion'
 import {containerVariant} from '../variable/MotionVariant'
 import UseWindowSmall from '../utilityhook/useWindowSmall'
 import Content from '../layout/Content'
@@ -49,18 +49,17 @@ const buttonVariant = {
     opacity: 1,
     transition: {
       ease: 'easeInOut',
-      duration: 0.7,
-      delay: 2
+      duration: 1,
+      delay: 2.5
     }
   }
 }
 
 const FriendSleep = () => {
-  const { friendInfoContext } = useUserStateContext()
+  const {friendInfoContext} = useUserStateContext()
   const isWindowSmall = UseWindowSmall()
 
   // state
-  const [skipAnimate, setSkipAnimate] = useState(false)
   const [animateComplete, setAnimateComplete] = useState(false)
   const [showModal, setShowModal] = useState(false)
 
@@ -73,83 +72,7 @@ const FriendSleep = () => {
     if (isWindowSmall) {
       if (animateComplete) {
         openModalFormFriend()
-      } else {
-        if (!skipAnimate) {
-          setAnimateComplete(false) // We will change to 'true' if We want to use function 'skip'
-          setSkipAnimate(false) // We will change to 'true' if We want to use function 'skip'
-        }
       }
-    }
-  }
-
-  // function for rendering
-  const renderBackground = () => {
-    if (isWindowSmall) {
-      return (
-        skipAnimate
-        ? <div className="friend-sleep__friend">
-            <img src={isWindowSmall ? ImgFriendSleepSm: ImgFriendSleepMd} alt="เพื่อนนอนสลบอยู่บนโต๊ะกินข้าว" />
-          </div>
-        : <AnimatePresence>
-            <motion.div className="friend-sleep__friend"
-              variants={friendVariant}
-              initial="hidden"
-              animate="show"
-              onAnimationComplete={ () => setAnimateComplete(true) }
-            >
-              <img src={isWindowSmall ? ImgFriendSleepSm: ImgFriendSleepMd} alt="เพื่อนนอนสลบอยู่บนโต๊ะกินข้าว" />
-            </motion.div>
-          </AnimatePresence>
-      )
-    } else {
-      return (
-        <motion.div className="friend-sleep__friend"
-          variants={friendVariant}
-          initial="hidden"
-          animate="show"
-        >
-          <img src={isWindowSmall ? ImgFriendSleepSm: ImgFriendSleepMd} alt="เพื่อนนอนสลบอยู่บนโต๊ะกินข้าว" />
-        </motion.div>
-      )
-    }
-  }
-
-  const renderText = () => {
-    if (isWindowSmall) {
-      return (
-        skipAnimate
-        ? <p className="box-story__text text-story stt">
-            หลังจากห้องสว่าง<br />คุณก็ได้พบกับเพื่อนสนิทของคุณ<br />หลับอยู่บนโต๊ะ
-          </p>
-        : <AnimatePresence>
-            <motion.p className="box-story__text text-story"
-              variants={textVariant}
-              initial="hidden"
-              animate="show"
-            >
-              หลังจากห้องสว่าง<br />คุณก็ได้พบกับเพื่อนสนิทของคุณ<br />หลับอยู่บนโต๊ะ
-            </motion.p>
-          </AnimatePresence>
-      )
-    } else {
-      return (
-        <>
-          <motion.p className="box-story__text text-story"
-            variants={textVariant}
-            initial="hidden"
-            animate="show"
-          >
-            หลังจากห้องสว่าง<br />คุณก็ได้พบกับเพื่อนสนิทของคุณ<br />หลับอยู่บนโต๊ะ
-          </motion.p>
-          <motion.div className="box-story__button"
-            variants={buttonVariant}
-            initial="hidden"
-            animate="show"
-          >
-            <ButtonNext onClick={openModalFormFriend} />
-          </motion.div>
-        </>
-      )
     }
   }
 
@@ -172,13 +95,32 @@ const FriendSleep = () => {
       <Content>
         <div className="scene-panel friend-sleep" onClick={touchPanelSm}>
           <div className="friend-sleep__text box-story">
+            <motion.p className="box-story__text text-story"
+              variants={textVariant}
+              initial={friendInfoContext.name? false: "hidden"}
+              animate="show"
+            >
+              หลังจากห้องสว่าง<br />คุณก็ได้พบกับเพื่อนสนิทของคุณ<br />หลับอยู่บนโต๊ะ
+            </motion.p>
             {
-              renderText()
+              !isWindowSmall
+              && <motion.div className="box-story__button"
+                  variants={buttonVariant}
+                  initial={friendInfoContext.name? false: "hidden"}
+                  animate="show"
+                >
+                  <ButtonNext onClick={openModalFormFriend} />
+                </motion.div>
             }
           </div>
-          {
-            renderBackground()
-          }
+          <motion.div className="friend-sleep__friend"
+            variants={friendVariant}
+            initial={friendInfoContext.name? false: "hidden"}
+            animate="show"
+            onAnimationComplete={() => setAnimateComplete(true)}
+          >
+            <img src={isWindowSmall ? ImgFriendSleepSm: ImgFriendSleepMd} alt="เพื่อนนอนสลบอยู่บนโต๊ะกินข้าว" />
+          </motion.div>
         </div>
       </Content>
       {
