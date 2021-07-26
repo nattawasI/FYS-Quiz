c167114d19400363daee7f20ab4c6cce8a9c979c
Prapat Prapatsornmanu(Tun) <54891620+prapat-psm@users.noreply.github.com>
2021-07-26 18:22:28 +0700

    fix: add viewport-extra.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './style/App.scss'
+import ViewportExtra from 'viewport-extra'
 import RouteProvider from './contexts/RouteContext'
 import UserProvider from './contexts/UserContext'
 import SoundProvider from './contexts/SoundContext'
 import Index from './pages/Index'
 
 const App = () => {
+  new ViewportExtra(375)
+
   return (
     <UserProvider>
       <RouteProvider>
