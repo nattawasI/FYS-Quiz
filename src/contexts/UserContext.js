fc76250793a9a14c5bbb8e214fe25ea845ed1eab
kungo000 <kungo2009@hotmail.com>
2021-07-28 00:05:19 +0700

    API Question and user data

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,85 +1,94 @@
-import React, {
-  useState,
-  createContext,
-  useContext
-} from 'react'
-
-const UserStateContext = createContext()
-const UserActionContext = createContext()
+import React, { useState, createContext, useContext } from "react";
+import { saveQuestion1, saveQuestion2, saveUser } from "../services";
+const UserStateContext = createContext();
+const UserActionContext = createContext();
 
 export const useUserStateContext = () => {
-  return useContext(UserStateContext)
-}
+  return useContext(UserStateContext);
+};
 
 export const useUserActionContext = () => {
-  return useContext(UserActionContext)
-}
+  return useContext(UserActionContext);
+};
 
 const UserProvider = ({ children }) => {
   // state
   // Phrase 1
-  const [friendInfoContext, setFriendNameContext] = useState({}) // ชื่อ และ เพศ ของเพื่อน
-  const [userInfoContext, setUserInfoContext] = useState({}) // ชื่อ และ เพศ ของคนเล่น
+  const [friendInfoContext, setFriendNameContext] = useState({}); // ชื่อ และ เพศ ของเพื่อน
+  const [userInfoContext, setUserInfoContext] = useState({}); // ชื่อ และ เพศ ของคนเล่น
 
+  const [userId, setUserId] = useState(0);
   // Phrase 2
-  const [activityOftenContext, setActivityOftenContext] = useState('') // กิจกรรมที่ทำบ่อยกับเพื่อน?
-  const [choicesContext, setChoicesContext] = useState([]) // คำถาม quiz และคำตอบที่ตอบ
+  const [activityOftenContext, setActivityOftenContext] = useState(""); // กิจกรรมที่ทำบ่อยกับเพื่อน?
+  const [choicesContext, setChoicesContext] = useState([]); // คำถาม quiz และคำตอบที่ตอบ
   // [{ id: '', question: '', label: '', score: 0 }, { id: '', question: '', label: '', score: 0 }, { id: '', question: '', label: '', score: 0 }]
 
-  const [yearsKnownContext, setYearsKnownContext] = useState('') // จำนวนปีที่รู้จัก?
-  const [activityTodayContext, setActivityTodayContext] = useState('') // กิจกรรมล่าสุด ที่ทำกับเพื่อน ก่อนเพื่อนตาย?
+  const [yearsKnownContext, setYearsKnownContext] = useState(""); // จำนวนปีที่รู้จัก?
+  const [activityTodayContext, setActivityTodayContext] = useState(""); // กิจกรรมล่าสุด ที่ทำกับเพื่อน ก่อนเพื่อนตาย?
 
   // Phrase 3
-  const [causeDiabetesContext, setCauseDiabetesContext] = useState('') // โรคเบาหวาน เกิดจากอะไร
-  const [symptomDiabetesContext, setSymptomDiabetesContext] = useState('') // โรคเบาหวาน มีอาการเป็นอย่างไร?
+  const [causeDiabetesContext, setCauseDiabetesContext] = useState(""); // โรคเบาหวาน เกิดจากอะไร
+  const [symptomDiabetesContext, setSymptomDiabetesContext] = useState(""); // โรคเบาหวาน มีอาการเป็นอย่างไร?
 
   /* function
   -------------------------------------------------------------- */
   // Phrase 1
   const addFriendInfoContext = ({ name, gender }) => {
-    const friendInfo = { name, gender }
-    setFriendNameContext(friendInfo)
-  }
+    const friendInfo = { name, gender };
+    setFriendNameContext(friendInfo);
+  };
 
   const addUserNameContext = (name) => {
-    setUserInfoContext({...userInfoContext, ...{ name }})
-  }
+    setUserInfoContext({ ...userInfoContext, ...{ name } });
+  };
 
-  const addUserGenderContext = (gender) => {
-    setUserInfoContext({...userInfoContext, ...{ gender }})
-  }
+  const addUserGenderContext = async (gender) => {
+    setUserInfoContext({ ...userInfoContext, ...{ gender } });
+    const id = await saveUser(friendInfoContext, userInfoContext, gender);
+    setUserId(id);
+  };
 
   // Phrase 2
   const addActivityOftenContext = (activity) => {
-    setActivityOftenContext(activity)
-    setChoicesContext([])
-  }
+    setActivityOftenContext(activity);
+    setChoicesContext([]);
+  };
 
   const addChoicesContext = (choice) => {
-    setChoicesContext([...choicesContext, choice])
-  }
+    setChoicesContext([...choicesContext, choice]);
+  };
 
   const removeChoicesContext = () => {
-    choicesContext.pop()
-    setChoicesContext(choicesContext)
-  }
+    choicesContext.pop();
+    setChoicesContext(choicesContext);
+  };
 
   const addYearsKnownContext = (years) => {
-    setYearsKnownContext(years)
-  }
+    setYearsKnownContext(years);
+  };
 
-  const addActivityTodayContext = (activity) => {
-    setActivityTodayContext(activity)
-  }
+  const addActivityTodayContext = async (activity) => {
+    setActivityTodayContext(activity);
+
+    await saveQuestion1(
+      activityOftenContext,
+      userId,
+      activity,
+      yearsKnownContext,
+      choicesContext
+    );
+  };
 
   // Phrase 3
   const addCauseDiabetesContext = (cause) => {
-    setCauseDiabetesContext(cause)
-  }
+    setCauseDiabetesContext(cause);
+  };
 
-  const addSymptomDiabetesContext = (symptom) => {
-    setSymptomDiabetesContext(symptom)
-  }
+  const addSymptomDiabetesContext = async (symptom) => {
+    setSymptomDiabetesContext(symptom);
+
+    await saveQuestion2(causeDiabetesContext, symptom, userId);
+  };
 
   const userStateStore = {
     // Phrase 1
@@ -95,7 +104,7 @@ const UserProvider = ({ children }) => {
     // Phrase 3
     causeDiabetesContext,
     symptomDiabetesContext,
-  }
+  };
 
   const userActionStore = {
     // Phrase 1
@@ -113,17 +122,17 @@ const UserProvider = ({ children }) => {
     // Phrase 3
     addCauseDiabetesContext,
     addSymptomDiabetesContext,
-  }
+  };
 
   return (
     <>
-      <UserStateContext.Provider value={ userStateStore }>
-        <UserActionContext.Provider value={ userActionStore }>
-          { children }
+      <UserStateContext.Provider value={userStateStore}>
+        <UserActionContext.Provider value={userActionStore}>
+          {children}
         </UserActionContext.Provider>
       </UserStateContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default UserProvider
+export default UserProvider;
---
+++ src/services/index.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+
+const url = "http://localhost:14080";
+const APIPOST = (url, data) => {
+  return axios.post(url, data, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+};
+
+export const saveUser = async (friendInfoContext, userInfoContext, gender) => {
+  const userData = {
+    friend_name: friendInfoContext.name,
+    friend_gen: friendInfoContext.name,
+    my_name: userInfoContext.name,
+    my_gen: gender,
+  };
+  const {
+    data: { id = 0 },
+  } = await APIPOST(url + "/userdata/save", userData);
+  return id;
+};
+
+export const saveQuestion1 = async (
+  activityOftenContext,
+  userId,
+  activity,
+  yearsKnownContext,
+  choicesContext
+) => {
+  const _choicesContext = [];
+  _choicesContext.push({
+    id: "1",
+    number: 1,
+    label: activityOftenContext,
+    question: "กิจกรรมที่ทำบ่อยกับเพื่อน?",
+    user_data_id: userId,
+
+    score: 0,
+  });
+  _choicesContext.push({
+    id: "2",
+    number: 2,
+    label: activity,
+    user_data_id: userId,
+
+    question: "กิจกรรมล่าสุด ที่ทำกับเพื่อน ก่อนเพื่อนตาย?",
+    score: 0,
+  });
+
+  _choicesContext.push({
+    id: "3",
+    number: 3,
+    user_data_id: userId,
+    label: yearsKnownContext,
+    question: "จำนวนปีที่รู้จัก?",
+    score: 0,
+  });
+  for (let i = 0; i < choicesContext.length; i++) {
+    const _c = choicesContext[i];
+    _choicesContext.push({
+      id: _c.id,
+      number: _choicesContext.length + 1,
+      user_data_id: userId,
+      label: _c.label,
+      question: _c.question,
+      score: _c.score,
+    });
+  }
+  // console.log({ _choicesContext });
+  await APIPOST(url + "/quizsubmit/save", {
+    datas: _choicesContext,
+  });
+};
+
+export const saveQuestion2 = async (causeDiabetesContext, symptom, userId) => {
+  const _choicesContext = [];
+  _choicesContext.push({
+    id: "7",
+    number: 7,
+    user_data_id: userId,
+    label: causeDiabetesContext,
+    question: "โรคเบาหวาน เกิดจากอะไร",
+    score: 0,
+  });
+  _choicesContext.push({
+    id: "8",
+    number: 8,
+    user_data_id: userId,
+    label: symptom,
+    question: "โรคเบาหวาน มีอาการเป็นอย่างไร?",
+    score: 0,
+  });
+  await APIPOST(url + "/quizsubmit/save", {
+    datas: _choicesContext,
+  });
+};
