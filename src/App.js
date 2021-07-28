4fd5b0f2fadb15b41cab750a96ff5a927b04a6e8
Prapat Prapatsornmanu(Tun) <54891620+prapat-psm@users.noreply.github.com>
2021-07-29 00:35:41 +0700

    fix: list-summary margin %

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './style/App.scss'
-import ViewportExtra from 'viewport-extra'
+// import ViewportExtra from 'viewport-extra'
 import RouteProvider from './contexts/RouteContext'
 import UserProvider from './contexts/UserContext'
 import SoundProvider from './contexts/SoundContext'
 import Index from './pages/Index'
 
 const App = () => {
-  new ViewportExtra(375)
+  // new ViewportExtra(375)
 
   return (
     <UserProvider>
