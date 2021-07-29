7c706e904c669e02e6fa7588946805d4d1c81b46
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-07-29 15:11:02 +0700

    fixed position start human

--- src/contexts/RouteContext.js
+++ src/contexts/RouteContext.js
@@ -13,7 +13,7 @@ export const useRouteActionContext = () => {
 
 const RouteProvider = ({children}) => {
   // state
-  const [currentPageContext, setCurrentPageContext] = useState('VideoDoctor')
+  const [currentPageContext, setCurrentPageContext] = useState('Start')
 
   // function
   const changeCurrentPageContext = (pageTarget) => {
