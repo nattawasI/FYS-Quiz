586aa12c16b540bdba252d4feaf744f77501d221
kungo000 <kungo2009@hotmail.com>
2021-07-31 19:01:24 +0700

    fix app

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     const redirectPage = () => {
       if (window.location.href.match(/female/)?.length)
         window.location.href =
-          "/https://www.foryoursweetheart.org/%E0%B8%86%E0%B8%B2%E0%B8%95%E0%B8%81%E0%B8%A3%E0%B8%9A%E0%B8%99%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/TH/";
+          "https://www.foryoursweetheart.org/%E0%B8%86%E0%B8%B2%E0%B8%95%E0%B8%81%E0%B8%A3%E0%B8%9A%E0%B8%99%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/TH/";
     };
     redirectPage();
   }, []);
