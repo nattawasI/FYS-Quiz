03cb2db9cb43532e678d5697ae743fc208f8ff7f
kungo000 <kungo2009@hotmail.com>
2021-07-31 18:53:08 +0700

    red

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import './style/App.scss'
-import RouteProvider from './contexts/RouteContext'
-import UserProvider from './contexts/UserContext'
-import SoundProvider from './contexts/SoundContext'
-import Index from './pages/Index'
+import React, { useEffect } from "react";
+import "./style/App.scss";
+import RouteProvider from "./contexts/RouteContext";
+import UserProvider from "./contexts/UserContext";
+import SoundProvider from "./contexts/SoundContext";
+import Index from "./pages/Index";
 
 const App = () => {
-
+  useEffect(() => {
+    const redirectPage = () => {
+      if (window.location.href.match(/female/)?.length)
+        window.location.href =
+          "/https://www.foryoursweetheart.org/%E0%B8%86%E0%B8%B2%E0%B8%95%E0%B8%81%E0%B8%A3%E0%B8%9A%E0%B8%99%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/TH/";
+    };
+    redirectPage();
+  }, []);
   return (
     <UserProvider>
       <RouteProvider>
@@ -16,6 +23,6 @@ const App = () => {
       </RouteProvider>
     </UserProvider>
   );
-}
+};
 
 export default App;
