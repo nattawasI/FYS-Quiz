f8ab91dfd8f40bf59c682b97fa375a15b060c916
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-08-02 13:01:10 +0700

    move code redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./style/App.scss";
 import RouteProvider from "./contexts/RouteContext";
 import UserProvider from "./contexts/UserContext";
@@ -6,14 +6,6 @@ import SoundProvider from "./contexts/SoundContext";
 import Index from "./pages/Index";
 
 const App = () => {
-  useEffect(() => {
-    const redirectPage = () => {
-      if (window.location.href.match(/female/)?.length)
-        window.location.href =
-          "/https://www.foryoursweetheart.org/%E0%B8%86%E0%B8%B2%E0%B8%95%E0%B8%81%E0%B8%A3%E0%B8%9A%E0%B8%99%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0%E0%B8%AD%E0%B8%B2%E0%B8%AB%E0%B8%B2%E0%B8%A3/TH/";
-    };
-    redirectPage();
-  }, []);
   return (
     <UserProvider>
       <RouteProvider>
