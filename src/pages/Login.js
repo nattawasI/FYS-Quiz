779575b6daa3d8378a0cadbedc6ef5a436d8a33b
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-08-04 01:50:31 +0700

    save login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -34,42 +34,46 @@ const Login = () => {
 
   return (
     <div className="login">
-      <div className="login__col login-banner">
-        <img src={ImgTitle} className="login-banner__title" alt="ฆาตกรบนโต๊ะอาหาร" />
-        <img src={ImgHumanSleep} className="login-banner__body" alt="" />
-      </div>
-      <div className="login__col">
-        <div className="login-form">
-          <div className="login-form__logo">
-            <img src={ImgLogo} alt="For Your Sweetheart" />
+      <div className="login__box">
+        <div className="login__col login__col--left">
+          <div className="login-banner">
+            <img src={ImgTitle} className="login-banner__title" alt="ฆาตกรบนโต๊ะอาหาร" />
+            <img src={ImgHumanSleep} className="login-banner__body" alt="" />
           </div>
-          <div className="login-form__title">Login</div>
-          <form className="login-form__form">
-            <div className="login-form__input">
-              <div className="login-input login-input--username">
-                <input type="text" placeholder="Username" />
+        </div>
+        <div className="login__col login__col--right">
+          <div className="login-form">
+            <div className="login-form__logo">
+              <img src={ImgLogo} alt="For Your Sweetheart" />
+            </div>
+            <div className="login-form__title">Login</div>
+            <form className="login-form__form">
+              <div className="login-form__input">
+                <div className="login-input login-input--username">
+                  <input type="text" placeholder="Username" />
+                </div>
               </div>
-            </div>
-            <div className="login-form__input">
-              <div className="login-input login-input--password">
-                <input type={showPassword? 'text': 'password'} placeholder="Password" />
-                <button
-                  type="button"
-                  className="login-input--password-toggle"
-                  onClick={togglePassword}
-                >
-                  {
-                    showPassword
-                    ? <img src={IconHiddenPassword} alt="Show Password" />
-                    : <img src={IconShowPassword} alt="Hidden Password" />
-                  }
-                </button>
+              <div className="login-form__input">
+                <div className="login-input login-input--password">
+                  <input type={showPassword? 'text': 'password'} placeholder="Password" />
+                  <button
+                    type="button"
+                    className="login-input--password-toggle"
+                    onClick={togglePassword}
+                  >
+                    {
+                      showPassword
+                      ? <img src={IconHiddenPassword} alt="Show Password" />
+                      : <img src={IconShowPassword} alt="Hidden Password" />
+                    }
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="login-form__button">
-              <button type="button" className="app-button" onClick={handleLoggedin}>Log in</button>
-            </div>
-          </form>
+              <div className="login-form__button">
+                <button type="button" className="app-button" onClick={handleLoggedin}>Log in</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
