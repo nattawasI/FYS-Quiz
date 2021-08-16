e42df4ffb6cab3a26d7cf972cd179befca89db43
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-08-16 19:46:50 +0700

    add button download

--- src/components/dashboard/Bar.js
+++ src/components/dashboard/Bar.js
@@ -2,11 +2,24 @@ import React from 'react'
 import DatePicker from './DatePicker'
 
 const Bar = () => {
+  // function
+  const handleDownload = () => {
+    console.log('Download!');
+  }
 
   return (
     <div className="app-bar app-container">
       <div className="app-bar__date">
         <DatePicker />
+      </div>
+      <div className="app-bar__button">
+        <button
+          type="button"
+          className="app-datepicker__button app-button app-button--download"
+          onClick={handleDownload}
+        >
+          ดาวน์โหลดตาราง
+        </button>
       </div>
     </div>
   )
