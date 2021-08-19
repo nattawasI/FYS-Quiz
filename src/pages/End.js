9050181a69b8bac89e99704a372a4612f117ddb8
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-08-19 23:35:54 +0700

    save for test

--- src/pages/End.js
+++ src/pages/End.js
@@ -107,10 +107,14 @@ const End = () => {
 
   const shareFacebook = async (e) => {
     e.preventDefault()
+    // const urlShare = encodeURIComponent(
+    //   `https://foryoursweetheart.org/ฆาตกรบนโต๊ะอาหาร/${
+    //     friendInfoContext.gender === "female" ? "female/" : ""
+    //   }TH`
+    // );
+
     const urlShare = encodeURIComponent(
-      `https://foryoursweetheart.org/ฆาตกรบนโต๊ะอาหาร/${
-        friendInfoContext.gender === "female" ? "female/" : ""
-      }TH`
+      `https://www.foryoursweetheart.org/TestQuiz/get_survey`
     );
 
     const app_id = '536732160711117'
