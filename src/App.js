19035ddf25f70138c662284a3e000db0f04c0c94
Nattawas Intaraprasit <nattawas@monosus.co.jp>
2021-08-19 22:19:37 +0700

    remove dashboard

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
 import React from "react"
 import "./style/App.scss"
-import {
-  // BrowserRouter as Router,
-  Switch,
-  Route,
-  HashRouter
-} from "react-router-dom"
 import RouteProvider from "./contexts/RouteContext"
 import UserProvider from "./contexts/UserContext"
 import SoundProvider from "./contexts/SoundContext"
-import DashboardProvider from "./contexts/DashboardContext"
 import Index from "./pages/Index"
-import Admin from "./pages/admin/Admin"
 
 const App = () => {
   return (
     <UserProvider>
       <RouteProvider>
         <SoundProvider>
-          <HashRouter>
-            <Switch>
-              <Route exact path="/">
-                <Index />
-              </Route>
-              <Route path="/admin/">
-                <DashboardProvider>
-                  <Admin />
-                </DashboardProvider>
-              </Route>
-            </Switch>
-          </HashRouter>
+          <Index />
         </SoundProvider>
       </RouteProvider>
     </UserProvider>
